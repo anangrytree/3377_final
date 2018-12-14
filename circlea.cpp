166531a17c2f46901a640a9d4d1889f67d9357b7
Seth Anthony Bedford <sab170730@cslinux1.utdallas.edu>
2018-12-14 13:56:20 -0600

    fixed issues with both approximations

--- circlea.cpp
+++ circlea.cpp
@@ -10,23 +10,31 @@ using namespace std;
 
 int main() {
 
-	double x, y;
-	int hits = 0;
+	double x, y, deviation;
+	int hits = 0, i;
 
-	for(int i = 0; i < 10; i++) {
-		gsl_rng * _gsl_rng = gsl_rng_alloc(gsl_rng_mt19937);
-		srand(time(NULL) ^ getpid());
-		gsl_rng_set(_gsl_rng, rand());
+	gsl_rng * _gsl_rng = gsl_rng_alloc(gsl_rng_mt19937);
+	srand(time(NULL) ^ getpid());
+	gsl_rng_set(_gsl_rng, rand());
+	for(i = 1; i <= 100; i++) {
 		x = gsl_rng_uniform(_gsl_rng);
 		y = gsl_rng_uniform(_gsl_rng);
-		gsl_rng_free(_gsl_rng);
 	
 		double distance = sqrt(pow(x, 2) + pow(y, 2));
 		if(distance <= 1) {
 			hits++;
 		}
 
-		cout << 4.0 * hits / i << endl;
+		deviation += pow((4.0 * hits / i) - 3.1415926535, 2);
+
+		cout << "Point #" << i << " ";
+		cout << (4.0 * hits) / i << endl;
 	}
+	gsl_rng_free(_gsl_rng);
+
+	cout << endl;
+	cout << "Approximation: " << 4.0 * hits / i << endl;
+	cout << "Deviation: " << sqrt(deviation / i) << endl;
+	cout << "% error: " << 100 * abs((4.0 * hits / i) - 3.1415926535) / 3.1415926535 << endl;
 	return 0;
 }
